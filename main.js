e8994df482d198c5bca5c46e1306ed12c2d2b732
Hassan <havhassanb@gmail.com>
2026-01-03 00:14:52 +0100

    add btn

--- main.js
+++ main.js
@@ -4,8 +4,21 @@ const authModal=document.querySelector(".auth-modal");
 const loginLink=document.querySelector(".login-link");
 const registerlink=document.querySelector(".register-link");
 
+const loginBtnModal=document.querySelector(".login-btn-modal");
+
+const closeBtnModal=document.querySelector(".close-btn-modal");
+
+const profileBox=document.querySelector(".profile-box");
+const avatarCircle=document.querySelector(".avatar-circle");
 
 
-registerlink.addEventListener("click",()=>
-      authModal.classList.add("slide"));
+registerlink.addEventListener("click",()=>authModal.classList.add("slide"));
 loginLink.addEventListener("click",()=> authModal.classList.remove('slide'));
+
+
+loginBtnModal.addEventListener("click",()=> authModal.classList.add('show'));
+
+closeBtnModal.addEventListener("click",()=> authModal.classList.remove('show',"slide"));
+
+
+avatarCircle.addEventListener("click",()=> profileBox.classList.toggle('show'));
